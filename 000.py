476079685d6b715f142d6a381e645bc94b5d8440
Coast8 <smythy.costa@gmail.com>
2020-06-07 09:04:56 -0300

    000

---
+++ 000.py
@@ -0,0 +1,15 @@
+
+
+
+https://cursos.alura.com.br/course/springboot
+https://cursos.alura.com.br/course/spring-boot-api-rest
+https://cursos.alura.com.br/course/spring-boot-seguranca-cache-monitoramento
+
+
+# Amazon
+https://cursos.alura.com.br/course/spring-amazon
+
+
+# Microserviços
+https://cursos.alura.com.br/course/microservices-spring-cloud-circuit-breaker-api-gateway
+https://cursos.alura.com.br/course/microservices-spring-cloud-service-registry-config-server
